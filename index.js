f127c49b1e425711529b17f10153bf2a24d6dd87
GokulS <gokul.s2024lcse@sece.ac.in>
2025-12-19 10:51:39 +0530

    fixing-2

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const cors = require("cors");
 const app = express();
 
 app.use(cors({
-  origin: "http://localhost:3000",
+  origin: ["http://localhost:3000", "https://frontend-nine-hazel-n0odiiy2v3.vercel.app"],
   credentials: true
 }));
 app.use(express.json());
